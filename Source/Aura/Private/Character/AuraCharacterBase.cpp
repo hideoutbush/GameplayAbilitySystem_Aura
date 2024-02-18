28c9b21278ea561945f27cf06198053007097d6b
hideoutbush <773903921@qq.com>
2024-02-18 16:18:24 +0800

    CharacterBlueprintSetup
    2/18

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,39 @@
+
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	//创建子组件名称
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	//设置无碰撞与蛇
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+// Called when the game starts or when spawned
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+//// Called every frame
+//void AAuraCharacterBase::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+//
+//// Called to bind functionality to input
+//void AAuraCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+//{
+//	Super::SetupPlayerInputComponent(PlayerInputComponent);
+//
+//}
+
---
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,35 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS(Abstract)
+class AURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAuraCharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	/// <summary>
+	/// 智能指针TObjectPtr无需在声明中添加*号
+	/// </summary>
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+
+//public:	
+//	// Called every frame
+//	virtual void Tick(float DeltaTime) override;
+//
+//	// Called to bind functionality to input
+//	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
